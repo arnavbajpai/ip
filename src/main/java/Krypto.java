1535cc806182502f06b18eeb161874d7b8fe75e0
Arnav Bajpai <arnavbcool@gmail.com>
2025-01-31 15:59:07 +0800

    Add Krypto

---
+++ src/main/java/Krypto.java
@@ -0,0 +1,6 @@
+public class Krypto {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Krypto \nWhat can I do for you ?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
