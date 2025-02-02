2ce43793cd98c4e1542e0dcb352bacdcfa53c9df
Arnav Bajpai <arnavbcool@gmail.com>
2025-02-03 03:59:37 +0800

    Add Echo

--- src/main/java/Krypto.java
+++ src/main/java/Krypto.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Krypto {
+   static  String line = "-".repeat(100);
+
+    public static void  printResponse(String input) {
+        System.out.println(line);
+        System.out.println(input);
+        System.out.println(line);
+    }
     public static void main(String[] args) {
+        System.out.println(line);
         System.out.println("Hello, I'm Krypto \nWhat can I do for you ?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+        Scanner myObj = new Scanner(System.in);
+        while(true) {
+            String prompt = myObj.nextLine();
+            if (prompt.equals("bye")) {
+                break;
+            }
+            printResponse(prompt);
+        }
+
     }
 }
