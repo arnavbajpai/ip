556ef51e218e6717448332a072d602c5d0349e41
Arnav Bajpai <arnavbcool@gmail.com>
2025-02-04 23:54:49 +0800

    Add Task

--- src/main/java/Krypto.java
+++ src/main/java/Krypto.java
@@ -2,34 +2,53 @@ import java.util.Scanner;
 public class Krypto {
    static String line = "-".repeat(100);
 
-
-
-    public static void  printResponse(String input) {
+    public static void  printResponse(Task input) {
         System.out.println(line);
         System.out.printf("Added :  %s\n", input);
         System.out.println(line);
     }
+
+    public static void printList(Task[] lst, int ctr) {
+        System.out.println("Here are the tasks in your list:");
+        for(int i = 0; i < ctr ; i ++) {
+            System.out.printf("%d. %s\n", i + 1, lst[i].toString());
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(line);
         System.out.println("Hello, I'm Krypto \nWhat can I do for you ?");
         System.out.println(line);
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int ctr = 0;
         Scanner myObj = new Scanner(System.in);
         while(true) {
             String prompt = myObj.nextLine();
+            String [] split = prompt.split(" ");
+            String first = split[0];
             if (prompt.equals("bye")) {
                 break;
             }
             else if (prompt.equals("list")) {
-                for(int i = 0; i < ctr ; i ++) {
-                    System.out.printf("%d. %s\n", i + 1, arr[i]);
-                }
+                printList(arr, ctr);
                 continue;
             }
-            arr[ctr] = prompt;
+            else if (first.equals("mark")) {
+                int id = Integer.parseInt(split[1]);
+                Task t = arr[id - 1];
+                t.markTask();
+                continue;
+            }
+            else if (first.equals("unmark")) {
+                int id = Integer.parseInt(split[1]);
+                Task t = arr[id - 1];
+                t.unmarkTask();
+                continue;
+            }
+            Task newTask = new Task(prompt);
+            arr[ctr] = newTask;
             ctr ++;
-            printResponse(prompt);
+            printResponse(newTask);
         }
 
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone = false;
+
+    private String sign = "";
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public void markTask() {
+        this.isDone = true;
+        this.sign = "X";
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(this.toString());
+    }
+    public void unmarkTask() {
+        this.isDone = false;
+        this.sign = "";
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(this.toString());
+    }
+    public String getStatusIcon() {
+        return this.sign;
+    }
+    @Override
+    public String toString() {
+        return "[" + this.sign + "] " + this.description;
+    }
 }
